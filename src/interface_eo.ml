ffadebf6e73651fd2789626833e8e06b29abdf7c
Ciarán Dunne <ciaran.dunne@inria.fr>
2025-11-17 19:11:52 -0300

    begin work on processing eo commands to signatures

--- src/interface_eo.ml
+++ src/interface_eo.ml
@@ -8,3 +8,24 @@ type judgement =
   | TypeJ of symbol * param list * term
   | DefnJ of symbol * param list * defn
   | AttrJ of symbol * param list * attr
+
+module SMap = Map.Make(String)
+type eo_sig = judgement SMap.t
+
+module Judgement = struct
+  type t = judgement
+  let compare = compare
+end
+
+module JSet = Set.Make(Judgement)
+type jset = JSet.t
+
+let proc_common_command : common_command -> JSet.t =
+  function
+  | DeclareConst     (s,t,xs)  -> failwith ""
+  | DeclareDatatype  (s,dt)    -> failwith ""
+  | DeclareDatatypes (sts,dts) -> failwith ""
+  | Echo             (str_opt) -> failwith ""
+  | Exit                       -> failwith ""
+  | Reset                      -> failwith ""
+  | SetOption        (a)       -> failwith ""
